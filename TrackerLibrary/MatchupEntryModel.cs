7e5b37d8ffb165b8b1da786ba412b5713ad6ed98
Yuyu Hsueh <joehsueh@hotmail.com>
2021-03-07 20:37:42 +0800

    First commit

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represent one team in the matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score for this particular team.
+        /// </summary>
+        public double Score { get; set; }
+        
+        /// <summary>
+        /// Represents the matchup that the team won and entered in this matchup
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }     
+    }
+}
